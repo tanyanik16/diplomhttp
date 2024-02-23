fd0828f741d58148a3969b8c5753e444b5c2e8a5
tanya_nik16 <tanya-nikiforova-2002@mail.ru>
2024-02-23 22:33:07 +0400

    #Проверка на существование документа в 1с

--- diplom1.0.py
+++ diplom1.0.py
@@ -62,66 +62,33 @@ def отправить_данныесправочник(datasp):
         print('Ошибка при отправке запроса:', e)
         return {'error': 'Ошибка при отправке запроса'}
 
-def получить_данныесправочникУслуги():
-    url = 'http://localhost/salon/hs/wdoc/service'
-    username = 'bromuser'
-    password = ''
 
-    response = requests.get(url, auth=(username, password))
-
-    if response.status_code == 200:
-        response_text = response.text
-        service = json.loads(response_text)
-        наименование_услуги = [item["Наименование"] for item in service]
-        return наименование_услуги
-    else:
-        print("Ошибка:", response.status_code)
-        return None
-
-def получить_данныесправочникСотрудники():
-    url = 'http://localhost/salon/hs/wdoc/master'
-    username = 'bromuser'
-    password = ''
-
-    response = requests.get(url, auth=(username, password))
-
-    if response.status_code == 200:
-        response_text = response.text
-        master = json.loads(response_text)
-        наименование_сотрудники = [item["Наименование"] for item in master]
-        return наименование_сотрудники
-    else:
-        print("Ошибка:", response.status_code)
-        return None
-
-def получить_данныедокументЗаписьДата():
+def получить_данные_документов():
     url = 'http://localhost/salon/hs/wdoc/note'
     username = 'bromuser'
     password = ''
     response = requests.get(url, auth=(username, password))
-    if response.status_code == 200:
-        response_text = response.text
-        data = json.loads(response_text)
-        дата_запись = [item["Дата"] for item in data]
-        return дата_запись
 
+    if response.status_code == 200:
+        return response.json()
     else:
-        print("Ошибка1:", response.status_code)
+        print("Ошибка3:", response.status_code)
+        return None
+def получить_данные_документа_Запись():
+    данные_документов = получить_данные_документов()
+    if данные_документов is None:
         return None
 
-def получить_данныедокументЗаписьСотрудник():
-    url = 'http://localhost/salon/hs/wdoc/note'
-    username = 'bromuser'
-    password = ''
-    response = requests.get(url, auth=(username, password))
-    if response.status_code == 200:
-        response_text = response.text
-        data = json.loads(response_text)
-        сотрудник_запись=[item["Сотрудник"] for item in data]
-        return сотрудник_запись
-    else:
-        print("Ошибка:", response.status_code)
-        return None
+    данные_запись = []
+
+    for документ in данные_документов:
+        данные_запись.append({
+            "Дата": datetime.strptime(документ["Дата"], "%Y-%m-%dT%H:%M:%SZ").strftime("%Y-%m-%dT%H:%M"),
+            "Сотрудник": документ["Сотрудник"]
+        })
+    return данные_запись
+
+
 
 #РАБОТА С WEB-САЙТОМ
 @app.route('/')
@@ -158,50 +125,33 @@ def submit():
     }
 
     #Проверка если документ сущшествует на текущую дату и время выводит сообщение в консоль об этом
-    дата1=получить_данныедокументЗаписьДата()
-    сотрудник=получить_данныедокументЗаписьСотрудник()
-    датаИсотрудникпреобраз=[datetime.strptime(дата, "%Y-%m-%dT%H:%M:%SZ").strftime("%Y-%m-%dT%H:%M") for дата in дата1]
+    #дата1=получить_данныедокументЗаписьДата()
+    #сотрудник=получить_данныедокументЗаписьСотрудник()
+   # датаИсотрудникпреобраз=[datetime.strptime(дата, "%Y-%m-%dT%H:%M:%SZ").strftime("%Y-%m-%dT%H:%M") for дата in дата1]
 
 
-
-    print(датаИсотрудникпреобраз)
-    print(сотрудник)
-    print(мастер)
-
-    if датаИВремя in датаИсотрудникпреобраз and мастер not in сотрудник:
-        print(f"Документ на  не существует1.{датаИВремя}{датаИсотрудникпреобраз}{мастер}{сотрудник}")
+    #Проверка на существование документа в 1с
+    данные_запись = получить_данные_документа_Запись()
+    документ_найден = False
+    for документ in данные_запись:
+        print(документ["Дата"])
+        print(датаИВремя)
+        if документ["Сотрудник"] == мастер and датаИВремя==документ["Дата"]:
+            документ_найден = True
+            break  # Прерываем цикл, если найден хотя бы один документ с указанным мастером
+    if документ_найден:
+        print("Документ существует!")
+    else:
+        print("Документ не существует!")
         результат = отправить_данные(data)
         результатsp = отправить_данныесправочник(datasp)
-        return jsonify(результат)
-        return jsonify(результатsp)
-    elif датаИВремя not in датаИсотрудникпреобраз and мастер in сотрудник:
-        print(f"Документ на текущую дату и время не существует2.{датаИВремя}{датаИсотрудникпреобраз}{мастер}{сотрудник}")
-        результат = отправить_данные(data)
-        результатsp = отправить_данныесправочник(datasp)
-        return jsonify(результат)
-        return jsonify(результатsp)
-    elif датаИВремя not in датаИсотрудникпреобраз and мастер not in сотрудник:
-        print(f"Документ на текущую дату и время не существует3.{датаИВремя}{датаИсотрудникпреобраз}{мастер}{сотрудник}")
-        результат = отправить_данные(data)
-        результатsp = отправить_данныесправочник(datasp)
-        return jsonify(результат)
-        return jsonify(результатsp)
-    elif датаИВремя in датаИсотрудникпреобраз and мастер  in сотрудник:
-        print('hdsjd')
-        return jsonify({"message": "Мастер ззанят на данную дату и время ."})
+        return jsonify(результатsp,результат)
 
-    # if датаИВремя in датаИсотрудникпреобраз:
-    #     print("Документ на текущую дату и время уже существует.")
-    # else:
-    #     print("Документ на текущую дату и время не существует.")
-    #     результат = отправить_данные(data)
-    #     результатsp = отправить_данныесправочник(datasp)
 
-    print("Отправляемые данные:", data)
-
+    #Загружаем данные в перечисление услугу и мастера на сайт
     наименование_услуги = получить_данныесправочникУслуги()
     наименование_сотрудники = получить_данныесправочникСотрудники()
-    # Отправляем данные в виде JSON
+
 
     #дОБАВЛЕНИЕ данных в гугл календарь
     # obj = GoogleCalendar()
@@ -221,6 +171,7 @@ def submit():
     # }
     # event= obj.add_event(calendar_id=calendar, body=event)
     # pprint.pp(obj.get_calendar_list())
+
 
 
 
